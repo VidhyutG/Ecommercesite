5602ed9da3dd149bae74ba30deb5d028c85a54a2
VidhyutG <89988331+VidhyutG@users.noreply.github.com>
2022-04-22 19:42:50 -0700

    Updated user schema

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
+
 require('dotenv').config();
+
+//import routes
+const userRoutes = require('./routes/user');
 
 //app
 const app = express();
@@ -11,10 +18,12 @@ mongoose
  .then(() => console.log("DB connected"))
  .catch((err) => console.log("DB error =>", err));
 
-//routes
-app.get('/',(req, res) => {
-  res.send('hello from node js');
-});
+//middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+//routes middleware
+app.use("/api", userRoutes)
 
 const port = process.env.PORT || 8000;
 
